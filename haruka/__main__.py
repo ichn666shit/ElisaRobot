5c10d5a6363074ac3fa8a0b271e865c80726f0bd
ichn666shit <75955944+ichn666shit@users.noreply.github.com>
2021-03-02 17:58:50 +0700

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,14 +22,12 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
-I'm a group manager bot maintained by @GarimaQueen.
+I'm a group manager bot maintained by Uyee FAMIGLIA!
 
-Add me to a group by [clicking here](http://t.me/ElisaRobot?startgroup=true).
+Add me to a group by [clicking here](http://t.me/UyeeFAMIGLIAbot?startgroup=true).
 
-🚨 Anti Ripping Organization 🚨
-You can join Fed or Subscribe to remove Rippers from your Groups [here](t.me/AntiRippingOrganization)
-
-"""
+🚨 Anyquestion? PM My Master! 🚨
+[clicking here](http://t.me/leeeeepuy).
 
 
 IMPORTED = {}
